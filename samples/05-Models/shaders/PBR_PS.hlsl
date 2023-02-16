86dba91ef16f738da767e1b028c68d1f371354bc
Andrey Shayda <shayda.andrey@yandex.ru>
2023-02-16 15:23:01 +0300

    Removed conversion func

--- samples/05-Models/shaders/PBR_PS.hlsl
+++ samples/05-Models/shaders/PBR_PS.hlsl
@@ -154,15 +154,6 @@ float3 LinearToSRGB( float3 x )
     return x < 0.0031308 ? 12.92 * x : 1.13005 * sqrt(abs(x - 0.00228)) - 0.13448 * x + 0.005719;
 }
 
-float3 SRGBToLinear( float3 x )
-{
-    // This is exactly the sRGB curve
-    //return x < 0.0031308 ? 12.92 * x : 1.055 * pow(abs(x), 1.0 / 2.4) - 0.055;
-
-    // This is cheaper but nearly equivalent
-    return x < 0.0031308 ? 12.92 * x : 1.13005 * sqrt(abs(x - 0.00228)) - 0.13448 * x + 0.005719;
-}
-
 float DoSpotCone( float3 spotDir, float3 L, float spotAngle )
 {
     float minCos = cos(spotAngle);
